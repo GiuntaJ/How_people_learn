fab2be0924dbbaa2a456ac79c8469ae716e98234
Julie Giunta <julie.giunta@epfl.ch>
2020-05-06 21:11:28 +0200

    All question stated in notebook

--- mcq.py
+++ mcq.py
@@ -11,6 +11,7 @@ def create_multipleChoice_widget(options, correct_answer, explanation, explanati
     alternativ = widgets.RadioButtons(
         options = radio_options,
         description = '',
+        layout={'width': 'max-content'},
         disabled = False
     )
         
@@ -59,7 +60,7 @@ def display_explanation(answer, explanation, explanation_gif):
           footer=None)
     display(output)
 
-def firstMCQQuestion():
+def MCQQuestion1():
     q4_options = ["A", "B", "C", "D"]
     q4_answer = "B"
     q4_explanation = "<b>Ball in circular motion</b>: the string applies the <b>centripetal force</b> " +\
@@ -74,3 +75,75 @@ def firstMCQQuestion():
     q4_solution_gif = "Gifs/solution.gif"
     q4 = create_multipleChoice_widget(q4_options, q4_answer, q4_explanation, q4_solution_gif)
     display(q4)
+    
+def MCQQuestion2():
+    q18_options = ["A", "B", "C", "D", "E"]
+    q18_answer = "B"
+    q18_explanation = "If the elevator is moving with <b>constant velocity</b>, the <b>net force must be zero (no acceleration)</b>. In order for the net force on the elevator to be zero, the upward and downward <b>forces must cancel</b> exactly."
+    q18_solution_gif = "Gifs/solution.gif"
+    q18 = create_multipleChoice_widget(q18_options, q18_answer, q18_explanation, q18_solution_gif)
+    display(q18)
+
+def MCQQuestion3():
+    q22_options = ["1 only", "1 and 2", "1, 2 and 3", "1 and 3", "2 and 3"]
+    q22_answer = "1 and 3"
+    q22_explanation = "The <i>hit</i> force is applied only for a short period during the <i>hit</i> and it gives the ball an <b>initial velocity</b>. Once the ball is <b>in flight</b>, the only forces acting on it are the <b>gravity</b>, which <b>accelerates</b> the ball <b>downwards</b>, and <b>air resistance</b>, which <b>decelerates</b> the ball."
+    q22_solution_gif = "Gifs/solution.gif"
+    q22 = create_multipleChoice_widget(q22_options, q22_answer, q22_explanation, q22_solution_gif)
+    display(q22)
+
+def MCQQuestion4():
+    q23_options = ["A", "B", "C", "D"]
+    q23_answer = "D"
+    q23_explanation = "As soon as the bowling ball leaves the plane, the <b>horizontal velocity is the same as the one of the plane</b> and the <b>vertical velocity is zero</b>. From that point onwards, the only forces acting on it are the <b>gravity</b> and the <b>air resistance</b>. The trajectory is therefore a <b>parabola</b>. The ball falling out of the plane is equivalent to throwing the ball with an initial horizontal velocity."
+    q23_solution_gif = "Gifs/solution.gif"
+    q23 = create_multipleChoice_widget(q23_options, q23_answer, q23_explanation, q23_solution_gif)
+    display(q23)
+
+def MCQQuestion5():
+    q28_options = ["If the force applied to the box is doubled, the constant speed of the box will increase to 8.0 m/s.", \
+        "The amount of force applied to move the box at a constant speed must be more than its weight.", \
+        "The amount of force applied to move the box at a constant speed must be equal to the amount of the frictional forces that resist its motion.", \
+        "The amount of force applied to move the box at a constant speed must be more than the amount of the frictional forces that resist its motion.", \
+        'There is a force being applied to the box to make it move but the external forces such as friction are not "real" forces, they just resist motion.']
+    q28_answer = "The amount of force applied to move the box at a constant speed must be equal to the amount of the frictional forces that resist its motion."
+    q28_explanation = "The box going at <b>constant speed</b>, means that the <b>acceleration is zero</b> and from the <b>second law of Newton</b>, it follows that the <b>sum of forces is also zero</b>."
+    q28_solution_gif = "Gifs/solution.gif"
+    q28 = create_multipleChoice_widget(q28_options, q28_answer, q28_explanation, q28_solution_gif)
+    display(q28)
+
+def MCQQuestion6():
+    q6_options = ["A", "B", "C", "D", "E"]
+    q6_answer = "B"
+    q6_explanation = "TODO"
+    q6_solution_gif = "Gifs/solution.gif"
+    q6 = create_multipleChoice_widget(q6_options, q6_answer, q6_explanation, q6_solution_gif)
+    display(q6)
+
+def MCQQuestion7():
+    q7_options = ['Equal to the speed “Vo” it had before it received the “kick”.', \
+        'Equal to the speed “V” it acquires from the “kick”, and independent of the speed “Vo”.', \
+        'Equal to the arithmetic sum of speeds “Vo” and “V”.', \
+        'Smaller than either of speeds “Vo” or “V”.', \
+        'Greater than either of speeds “Vo” or “V”, but smaller than the arithmetic sum of these two speeds.']
+    q7_answer = 'Greater than either of speeds “Vo” or “V”, but smaller than the arithmetic sum of these two speeds.'
+    q7_explanation = "TODO"
+    q7_solution_gif = "Gifs/solution.gif"
+    q7 = create_multipleChoice_widget(q7_options, q7_answer, q7_explanation, q7_solution_gif)
+    display(q7)
+
+def MCQQuestion8():
+    q24_options = ["A", "B", "C", "D", "E"]
+    q24_answer = "E"
+    q24_explanation = "TODO"
+    q24_solution_gif = "Gifs/solution.gif"
+    q24 = create_multipleChoice_widget(q24_options, q24_answer, q24_explanation, q24_solution_gif)
+    display(q24)
+
+def MCQQuestion9():
+    q26_options = ["A", "B", "C", "D", "E"]
+    q26_answer = "B"
+    q26_explanation = "TODO"
+    q26_solution_gif = "Gifs/solution.gif"
+    q26 = create_multipleChoice_widget(q26_options, q26_answer, q26_explanation, q26_solution_gif)
+    display(q26)
